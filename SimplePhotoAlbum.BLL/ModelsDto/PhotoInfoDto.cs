71617a891efdb3288142f315c79fd17154b33808
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-10 17:09:23 +0200

    Add BLL with stubs

---
+++ SimplePhotoAlbum.BLL/ModelsDto/PhotoInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SimplePhotoAlbum.BLL.ModelsDto
+{
+    public class PhotoInfoDto
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+    }
+}
