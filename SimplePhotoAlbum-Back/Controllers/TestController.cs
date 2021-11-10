35dc1f708c5a356f14add9103ab80082699f29a5
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-10 10:44:56 +0200

    Add TestController

---
+++ SimplePhotoAlbum-Back/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SimplePhotoAlbum_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("G");
+        }
+    }
+}
