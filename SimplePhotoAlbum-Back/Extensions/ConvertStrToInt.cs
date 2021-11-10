d08712c774b038845c85a43efabadb1ac3aeed5d
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-10 15:21:02 +0200

    Add extention for convert Str to INT with default value

---
+++ SimplePhotoAlbum-Back/Extensions/ConvertStrToInt.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplePhotoAlbum_Back.Extensions
+{
+    public static class ConvertStrToInt
+    {
+        public static int StrToIntDefault(this string inString, int defaultValue = 0)
+        {
+            return int.TryParse(inString, out int result) ? result : defaultValue;
+        }
+    }
+}
