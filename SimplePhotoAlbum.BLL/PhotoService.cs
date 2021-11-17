84a70d86df8be818abecd519a25fc7ed30e0a632
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-17 15:56:32 +0200

    Remove testing stub

--- SimplePhotoAlbum.BLL/PhotoService.cs
+++ SimplePhotoAlbum.BLL/PhotoService.cs
@@ -70,45 +70,5 @@ namespace SimplePhotoAlbum.BLL
             await _unitOfWork.PhotoRepository.UpdateAsync(photoInfo);
             await _unitOfWork.SaveAsync();
         }
-        /*
-        public List<PhotoInfoDto> GetPhotosInfo(int pageSize, int pageN)
-        {
-            PhotoInfoDto photoInfoDto = new PhotoInfoDto() { Id = 1, Caption = "img 1", Description = "img 1 desc" };
-            return new List<PhotoInfoDto>() { photoInfoDto }; 
-        }
-
-        public PhotoInfoDto GetPhotoInfoById(int id)
-        {
-            PhotoInfoDto photoInfoDto = new PhotoInfoDto() { Id = 1, Caption = "img 1", Description = "img 1 desc" };
-            return photoInfoDto;
-        }
-
-        public PhotoImageDto GetImageById(int id)
-        {
-            PhotoImageDto photoImageDto = new PhotoImageDto() { Id = 1, FileName = "17joke.jpg", ImageType="jpg" };
-            photoImageDto.Image = File.ReadAllBytes("C:\\tmp\\17joke.jpg");
-            return photoImageDto;
-
-        }
-
-        public PhotoInfoDto SavePhoto(PhotoInfoDto photoInfoDto, PhotoImageDto photoImageDto)
-        {
-            //if (photoImageDto.Image.Length > 1000) && (photoInfoDto.Caption.Length > 0);
-            PhotoInfoDto resultPhotoInfo = new PhotoInfoDto();
-            //Save PhotoInfo
-            //Save PhotoImage
-            return resultPhotoInfo;
-        }
-
-        public int GetCountPhotos()
-        {
-            return 43;
-        }
-
-        public bool DeletePhoto(int id)
-        {
-            return true;
-        }
-        */
     }
 }
